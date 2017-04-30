09ef7587d5764f3e249a0e227316d4612c9c16e5
Paul Bradshaw <paulonhismobile@gmail.com>
2017-04-30 09:27:14 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,28 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#import our libraries
+import urllib, json
+import scraperwiki
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+#a JSON file
+url = "https://data.police.uk/api/forces"
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+#fetch the json from the URL
+response = urllib.urlopen(url)
+#load it into variable called x
+data = json.loads(response.read())
+#let's see what we have
+print data
+print len(data)
+
+#create empty dictionary variable to hold our data
+record = {}
+#loop through each item in the data variable
+for i in data:
+    print i
+    print i['name']
+    if i['id'] == 'wiltshire':
+        print 'THIS IS WILTSHIRE2'
+    if 'wilt' in i['id']:
+        print 'IT CONTAINS WILT'
+    record['idcode'] = i['id']
+    record['fullname'] = i['name']
+    scraperwiki.sqlite.save(['idcode'], record)
